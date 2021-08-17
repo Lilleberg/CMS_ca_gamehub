15afac9eb65e41a4a844034cc5ce3b58323e7aaf
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-08-17 19:14:27 +0200

    div edits

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const containerUsed = document.querySelector(".used-games");
+const containerNew = document.querySelector(".new-releases");
+const mainSection = document.querySelector(".main-section");
+
+async function getProducts(url) {
+
+  try {
+    const response = await fetch(url);
+    const products = await response.json();
+    console.log(products);
+
+    for (let i = 0; i < products.length; i++) {
+      const game = products[i];
+
+      let id = i;
+
+      if (game.tags[0].name === "used") {
+        containerUsed.innerHTML +=
+          `<div class="game">
+          <a href="product.html?id="${id}"><img src="${game.images[0].src}" class="img-product"></a>
+          <div class="sub-content">
+            <a href="#">${game.name}</a>
+            <p class="price">${game.price_html}</p>
+            <button class="button add-to-cart" data-game="${game.name}">Add to cart</button>
+          </div>
+        </div>`;
+      }
+
+      if (game.tags[0].name === "new") {
+        containerNew.innerHTML +=
+          `<div class="game">
+          <a href="product.html?id="${id}"><img src="${game.images[0].src}" class="img-product"</a>
+          <div class="sub-content">
+            <a href="product.html?id=${id}">${game.name}</a>
+            <p class="price">${game.price_html}</p>
+            <button class="button add-to-cart" data-game="${game.name}">Add to cart</button>
+          </div>
+        </div>`;
+      }
+    }
+  } catch (error) {
+    console.log("ERROR:" + error);
+    mainSection.innerHTML = errorMessage();
+  }
+}
+
+getProducts("http://10.0.0.20/wordpress/wp-json/wc/store/products");
---
+++ js/product.js
@@ -0,0 +1,69 @@
+const productContainer = document.querySelector(".main-content");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+if (!id) {
+  location.href = "index.html";
+}
+
+const url = "http://10.0.0.20/wordpress/wp-json/wc/store/products" + id;
+
+async function getProduct() {
+
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    console.log(results);
+
+    document.title = `Game Hub | ${results.name}`;
+    productContainer.innerHTML +=
+      `<div class="page-title">
+        <h1>${results.name}</h1>
+        <p>${results.category}<p>
+      </div>
+      <div class="game-info>">
+        <img src="${}" alt="Product image of ${results.name}">
+        <h2>Information</h2>
+        <p>${results.userDescription}</p>
+      </div>
+      <div class="payment-info">
+        <p class="price">${results.price}</p>
+        <button class="button add-to-cart" data-game="CoD: Black Ops" data-cost="150">Add to cart</button>
+        <div>
+          <p>Payment methods</p>
+          <p>Credit card</p>
+          <p>Debit card</p>
+          <p>Vipps</p>
+        </div>
+      </div>
+      <div class="about-seller">
+        <div>
+          <h3>About seller</h3>
+          <p>${results.seller}</p>
+        </div>
+        <div class="member-since">
+          <p>Member since:</p>
+          <p>09.07.2017</p>
+        </div>
+        <div>
+          <p class="rating">Rating</p>
+          <div class="rating star">
+            <p><i class="fas fa-star"></i></p>
+            <p><i class="fas fa-star"></i></p>
+            <p><i class="fas fa-star"></i></p>
+            <p><i class="fas fa-star"></i></p>
+            <p><i class="fas fa-star-half-alt"></i></p>
+          </div>
+        </div>
+        <div class="message">
+          <p><i class="fas fa-comment-alt"></i></p>
+          <p>Message seller</p>
+        </div>
+      </div>`;
+  } catch (error) {
+    console.log("ERROR:" + error);
+    mainSection.innerHTML = errorMessage();
+  }
+}
