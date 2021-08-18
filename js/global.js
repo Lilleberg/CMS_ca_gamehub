ac14fa72a11dcec99d28e9748a89b9ed4870d49a
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-08-19 00:15:38 +0200

    div edits

---
+++ js/global.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".navbar");
+const hambIcon = document.querySelector("#hamburger-menu");
+
+hambIcon.onclick = function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
